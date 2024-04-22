dabec4cb5d485f50c2d6afdd1bc5e32115c2593a
Tanat Arora <tanat.navin@gmail.com>
2024-04-22 15:24:12 +0700

    try adding cache to solve blogs not updated

--- services/getAllBlogs.ts
+++ services/getAllBlogs.ts
@@ -1,19 +1,7 @@
 import { client } from "@/sanity/lib/client";
 import { IBlogCardType } from "@/types";
 
-// // Modify your getAllBlogs function to implement caching with a time-based expiration
-// let cachedData: IBlogCardType[] | null = null;
-// let lastFetchTime: number | null = null;
-// const CACHE_DURATION: number = 10 * 1000; // 10 seconds
-
-
 export default async function getAllBlogs() {
-  // // Check if data is cached and within expiration time
-  // if (cachedData && lastFetchTime && Date.now() - lastFetchTime < CACHE_DURATION) {
-  //   return cachedData;
-  // }
-
-  // Fetch fresh data from your API
   const query = `
     *[_type == 'post'] | order(publishedAt desc) {
       title,
@@ -26,11 +14,12 @@ export default async function getAllBlogs() {
       },
     }`;
 
-  const data = await client.fetch(query);
-
-  // // Cache the fetched data and update the last fetch time
-  // cachedData = data;
-  // lastFetchTime = Date.now();
+  // Opt out of caching for this fetch request using headers
+  const data = await client.fetch(query, undefined, {
+    headers: {
+      'Cache-Control': 'no-store',
+    },
+  });
 
   return data;
 }
