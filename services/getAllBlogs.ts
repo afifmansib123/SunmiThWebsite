7dd971bb49e6e3390e0df53266564ab1f207272e
Tanat Arora <tanat.navin@gmail.com>
2024-05-06 13:36:30 +0700

    update blogs

--- services/getAllBlogs.ts
+++ services/getAllBlogs.ts
@@ -1,7 +1,15 @@
 import { client } from "@/sanity/lib/client";
 import { IBlogCardType } from "@/types";
-
+// Modify your getAllBlogs function to implement caching with a time-based expiration
+let cachedData: IBlogCardType[] | null = null;
+let lastFetchTime: number | null = null;
+const CACHE_DURATION: number = 10 * 1000; // 10 seconds
 export default async function getAllBlogs() {
+  // Check if data is cached and within expiration time
+  if (cachedData && lastFetchTime && Date.now() - lastFetchTime < CACHE_DURATION) {
+    return cachedData;
+  }
+  // Fetch fresh data from your API
   const query = `
     *[_type == 'post'] | order(publishedAt desc) {
       title,
@@ -13,13 +21,9 @@ export default async function getAllBlogs() {
         title
       },
     }`;
-
-  // Opt out of caching for this fetch request using headers
-  const data = await client.fetch(query, undefined, {
-    headers: {
-      'Cache-Control': 'no-store',
-    },
-  });
-
+  const data = await client.fetch(query);
+  // Cache the fetched data and update the last fetch time
+  cachedData = data;
+  lastFetchTime = Date.now();
   return data;
 }
