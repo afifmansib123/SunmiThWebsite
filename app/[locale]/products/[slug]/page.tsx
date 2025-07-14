75bc259c76167fdc1f172e4241c75a288dc2d2cc
Tanat Arora <tanat.navin@gmail.com>
2025-07-14 09:31:05 +0700

    Add console log for debugging in PDFDJ component

--- app/[locale]/products/[slug]/page.tsx
+++ app/[locale]/products/[slug]/page.tsx
@@ -4,12 +4,13 @@ import getSingleProduct from "@/services/getSingleProduct";
 export const revalidate = 30; // revalidate at most 30 seconds
 
 const PDFDJ = async ({ params }: { params: { slug: string } }) => {
-  const item = await getSingleProduct(params.slug);
-  return (
-    <div>
-      <ProductDetails pdf={item?.pdfUrl || "/404.pdf"} />
-    </div>
-  );
+	console.log("params", params.slug);
+	const item = await getSingleProduct(params.slug);
+	return (
+		<div>
+			<ProductDetails pdf={item?.pdfUrl || "/404.pdf"} />
+		</div>
+	);
 };
 
 export default PDFDJ;
